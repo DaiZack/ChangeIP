3c4d18b45c1d85e6c4bac7d17a5913b1a39ac7b4
Zack Dai <45176371+zdrel8ed@users.noreply.github.com>
2019-07-13 21:18:33 -0400

    Update changeIP.py

--- changeIP.py
+++ changeIP.py
@@ -1,25 +1,38 @@
+import time
 badip = []
+usedip = []
 
-def changeIP(ipnow=''):
+def changeIP(usedip=[]):
     from bs4 import BeautifulSoup
     import requests
     url = 'https://free-proxy-list.net/'
     r = requests.get(url).content
     soup = BeautifulSoup(r)
-    rows = soup.select('tr')[1:20]
+    rows = soup.select('tr')[1:]
     ips = [r.select('td')[0].text+':'+r.select('td')[1].text for r in rows]
     url = 'https://httpbin.org/ip'
     for ip in ips:
-        if ip != ipnow and ip not in badip:
+        if ip in usedip and ip not in badip:
             try:
                 response = requests.get(url,proxies={"http": ip, "https": ip})
-                print(response.json())
-                print(ip)
-                return ip
+                if response.json():
+                    print(ip)
+                    return ip
             except:
                 print('skip :',ip)
                 badip.append(ip)
                 continue
 
-newip = changeIP('165.22.114.11:8080')
-print('ipchanged to :', newip)
+t0 = time.time()
+for i in range(10):
+    # do something
+    time.sleep(3)
+    t1 = time.time()
+    if t1-t0>10:
+        newip = changeIP(usedip)
+        print('ipchanged to :', newip)
+        if len(usedip)>9:
+            usedip = []
+        usedip.append(newip)
+        t0=t1
+    
